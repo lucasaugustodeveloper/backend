b5d01d4c1f4f757f27dac4bda67581c99d4c5927
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2024-03-15 11:56:44 -0300

    ♻️ (user): Add method save of user

--- api/user.js
+++ api/user.js
@@ -1,9 +1,94 @@
+const bcrypt = require('bcrypt-nodejs');
+
 module.exports = (app) => {
-  const save = (req, res) => {
-    res.send('user save');
+  const { existsOrError, notExistsOrError, equalsOrError } = app.api.validation;
+
+  const encryptPassword = (password) => {
+    const salt = bcrypt.genSaltSync(10);
+
+    return bcrypt.hashSync(password, salt);
+  };
+
+  const save = async (req, res) => {
+    const user = { ...req.body };
+
+    if (req.params.id) user.id = req.params.id;
+
+    try {
+      existsOrError(user.name, 'Name is required');
+      existsOrError(user.email, 'Email is required');
+      existsOrError(user.password, 'Password is required');
+      existsOrError(user.confirmPassword, 'Confirm Password is required');
+      equalsOrError(
+        user.password,
+        user.confirmPassword,
+        'Passwords do not match'
+      );
+
+      const userFromDB = await app
+        .db('users')
+        .where({
+          email: user.email,
+        })
+        .first();
+
+      if (!user.id) notExistsOrError(userFromDB, 'User exists');
+    } catch (err) {
+      res.status(400).send({
+        status: 400,
+        data: err,
+      });
+    }
+
+    user.password = encryptPassword(user.password);
+    delete user.confirmPassword;
+
+    if (user.id) {
+      return app
+        .db('users')
+        .update(user)
+        .where({ id: user.id })
+        .then(() => res.status(204).send())
+        .catch((err) =>
+          res.status(500).send({
+            status: 500,
+            data: err,
+          })
+        );
+    }
+
+    return app
+      .db('users')
+      .insert(user)
+      .then(() => res.status(204).send())
+      .catch((err) => {
+        res.status(500).send({
+          status: 500,
+          data: err,
+        });
+      });
+  };
+
+  const getAllUsers = (req, res) => {
+    app
+      .db('users')
+      .select('id', 'name', 'email', 'admin')
+      .then((users) =>
+        res.json({
+          status: 200,
+          data: users,
+        })
+      )
+      .catch((err) => {
+        res.status(500).send({
+          status: 500,
+          data: err,
+        });
+      });
   };
 
   return {
+    getAllUsers,
     save,
   };
 };
--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,5 @@
 module.exports = (app) => {
-  app.route('/users').post(app.api.user.save);
+  app.route('/users').post(app.api.user.save).get(app.api.user.getAllUsers);
+
+  app.route('/users/:id').put(app.api.user.save);
 };
