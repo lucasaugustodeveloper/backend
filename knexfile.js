300583c864fc396fa61c5937bfc2b77b448e1d90
Lucas Augusto Silva <lucas.augusto5061@gmail.com>
2024-03-26 21:26:44 -0300

    ♻️ (knex): Add ssl in knex

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,7 @@ module.exports = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    ssl: true,
   },
   searchPath: ['knex', 'public'],
   pool: {
